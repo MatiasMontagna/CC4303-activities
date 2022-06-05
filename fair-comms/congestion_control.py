464f47cc274c5e053b8eeedf9c88c283e68fcba0
Matías Montagna Carbonell <mmc@pop-os.localdomain>
2022-06-05 00:06:17 -0400

    added CongestionControl class

---
+++ fair-comms/congestion_control.py
@@ -0,0 +1,40 @@
+class CongestionControl:
+    def __init__(self, mss):
+        if  not isinstance(mss,int):
+            raise Exception("ERROR in CongestionControl, init(): mss must be an integer")
+
+        self.possible_states = ["slow_start", "congestion_avoidance"]
+        self.state = self.possible_states[0]
+        self.first_timeout = True
+        self.mss = mss
+        self.cwnd = mss
+        self.ssthresh = None
+
+    def event_ack_received(self):
+        '''Handles reception of ACKs according to current state'''
+        if self.state == self.possible_states[0]:
+            self.cwnd += self.mss
+            if self.ssthresh == None:
+                return
+
+            elif self.state >= self.ssthresh: 
+                self.state = self.possible_states[1]
+                self.first_timeout = True
+
+        elif self.state == self.possible_states[1]:
+            self.cwnd += self.mss/self.cwnd
+
+    def event_timemout(self):
+        '''Handles timeouts according to current state'''
+        if self.state == self.possible_states[0] and self.first_timeout:
+            self.ssthresh = self.cwnd/2
+            self.cwnd = self.mss
+            self.first_timeout = False
+
+        elif self.state == self.possible_states[1]:
+            self.state = self.possible_states[0]
+            self.ssthresh = None
+
+    def get_cwnd(self):
+        return self.cwnd
+            
